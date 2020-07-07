b6051fabb12294ae6429ac878e0f9408085a1a74
Ta Hong Anh <Ta Anh>
2020-07-07 18:10:38 +0700

    add css+login

--- login.php
+++ login.php
@@ -18,32 +18,24 @@
             
             <div class="header">
                 <img src="assets/images/vnflix.png" title="Logo" alt="Site logo">
-                <h3>Sign Up</h3>
+                <h3>Sign In</h3>
                 <span> to continue  to VNFLIX</span>
                 
             </div>
         
             <form action="" method="POST">
                 
-                <input type="text" name="firstName" id="" placeholder="First Name" required>
-
-                <input type="text" name="lastName" id="" placeholder="Last Name" required> 
+                
 
                 <input type="text" name="username" id="" placeholder="User Name" required> 
 
-                <input type="email" name="email" id="" placeholder="Email" required>
-
-                <input type="email" name="email2" id="" placeholder="Confirm Email" required>
-
                 <input type="password" name="password" id="" placeholder="Password" required>
-
-                <input type="password" name="password2" id="" placeholder="Confirm Password" required>
 
                 <input type="submit" name="submitButton" id="" value="SUBMIT">
             
             </form>
 
-            <a href="login.php" class="signInMessage"> Already have an account ? Sign In Here !</a>
+            <a href="register.php" class="signInMessage"> Need an account ? Sign Up Here !</a>
 
 
         </div>
